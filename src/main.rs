558697fa0ed3bc021b5541199ca9924c9d5536e6
João M. Bezerra <marcospb19@hotmail.com>
2023-09-27 14:27:26 -0300

    Add initial CLI interface

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::{env, path::PathBuf};
+use std::env;
 
+use clap::Parser;
 use dotin::{remove_links, utils::get_home_dir};
 
-fn main() {
-    env::set_current_dir("/home/marcospb19/dotfiles").unwrap();
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+enum Command {
+    Unlink { groups: Vec<String> },
+    Link { groups: Vec<String> },
+}
 
-    let path = PathBuf::from("i3");
+fn main() {
+    env::set_current_dir("/home/marcospb19").unwrap();
+
     let home_dir = get_home_dir();
 
-    remove_links(home_dir, &path).unwrap();
+    match Command::parse() {
+        Command::Unlink { groups } => {
+            for group in groups {
+                remove_links(home_dir, &group).unwrap();
+            }
+        }
+        Command::Link { groups: _ } => {
+            todo!("link implementation");
+        }
+    }
 }
